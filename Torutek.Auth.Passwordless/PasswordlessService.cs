3b90b5bd125c537ea8a185fdf1455675b71652e9
dave <danzel@localhost.geek.nz>
2017-08-29 14:02:56 +1200

    Build nuget packages

--- Torutek.Auth.Passwordless/PasswordlessService.cs
+++ Torutek.Auth.Passwordless/PasswordlessService.cs
@@ -6,6 +6,9 @@ using Microsoft.Extensions.Caching.Distributed;
 
 namespace Torutek.Auth.Passwordless
 {
+	/// <summary>
+	/// Manages Passwordless Authentication Concerns
+	/// </summary>
 	public interface IPasswordlessService
 	{
 		/// <summary>
